ff9336cbb386b474575553a02d2954cbda6f2f04
Francisco Possetto <franciscopossetto@hotmail.com>
2023-02-26 21:05:51 -0500

    minor changes in app.js and package json modifications

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.min.js";
+import React from "react";
+import { Provider } from "react-redux";
 import { AddFiction } from "./pages/AddFiction";
+import { store } from "./store/store";
 
 function App() {
-  return <AddFiction />;
+  return (
+    <React.StrictMode>
+      <Provider store={store}>
+        <AddFiction />
+      </Provider>
+    </React.StrictMode>
+  );
 }
 
 export default App;
