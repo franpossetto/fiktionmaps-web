a78685c1c92e29e34c7912bbdd6f7ce9f8c7c60f
Francisco Possetto <franciscopossetto@hotmail.com>
2023-03-26 13:07:21 -0400

    added context

---
+++ src/context/FictionContext.ts
@@ -0,0 +1,4 @@
+import React from 'react';
+
+const FictionContext = React.createContext<any[]>([]);
+export default FictionContext;
---
+++ src/types/IFictionPlaceContext.ts
@@ -0,0 +1,4 @@
+export interface IFictionPlaceConext {
+    fiction: any,
+    place: any
+}
