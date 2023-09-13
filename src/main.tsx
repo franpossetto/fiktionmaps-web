49e84901bcafeb5b8dea00507a550873516e81c5
DESKTOP-23A2GIA\Usuario <franciscopossetto@hotmail.com>
2023-09-13 17:47:13 -0300

    added fiction context

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { MapContext } from "@react-google-maps/api";
+import { MapController } from "./contexts/MapContext";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <App />
+  <MapController>
+    <App />
+  </MapController>
 );
