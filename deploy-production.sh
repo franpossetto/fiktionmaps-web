17e065cfec9fcc6b6ea81c73aef3e9a48cccb0d5
TATICO\franc <franciscopossetto@hotmail.com>
2024-02-14 15:29:24 -0500

    added file to deploy

--- deploy-production.sh
+++ deploy-production.sh
@@ -2,4 +2,4 @@
 #upload files
 aws --profile fpossetto s3 cp ./dist s3://www.fiktionmaps.com --recursive --acl public-read --region us-east-2 --exclude "" --include ".js" --include "..js"
 aws --profile fpossetto s3 cp ./dist s3://www.fiktionmaps.com --recursive --acl public-read --region us-east-2 --exclude ".js" --exclude ".*.js"
-# aws --profile fpossetto cloudfront create-invalidation --distribution-id CLOUDFRONT_ID --path "/*"
+aws --profile fpossetto cloudfront create-invalidation --distribution-id E3COPAL5XU2AB7 --path "/*"
