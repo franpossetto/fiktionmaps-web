c452666a95b27a8a2f10e73730db80cf2318b1a0
TATICO\franc <franciscopossetto@hotmail.com>
2024-03-23 10:10:11 -0300

    fixed cityService

--- src/services/useCityService.tsx
+++ src/services/useCityService.tsx
@@ -1,6 +1,12 @@
-import { axiosWithToken } from "../config/axios";
+import { useState } from "react";
+import { axiosWithToken, axiosWithoutToken } from "../config/axios";
 import { useAxios, useAxiosResponse } from "../config/useAxios";
-
+import {
+  AxiosError,
+  AxiosInstance,
+  AxiosResponse,
+  RawAxiosRequestConfig,
+} from "axios";
 export const useCityService = () => {
   const getCities = (): useAxiosResponse<any> => {
     return useAxios({
@@ -10,9 +16,9 @@ export const useCityService = () => {
     });
   };
 
-  const getCityById = (cityId: number): useAxiosResponse<any> => {
+  const getCityById = (cityId?: number): useAxiosResponse<any> => {
     return useAxios({
-      url: `/cities/${cityId}`,
+      url: `/cities/${cityId || 1}`,
       config: { method: "get" },
       tokenRequired: false,
     });
@@ -22,9 +28,21 @@ export const useCityService = () => {
     return axiosWithToken.delete(`/cities/${cityId}`);
   };
 
+  const getCities2 = (): Promise<AxiosResponse<any>> => {
+    let axiosInstance: AxiosInstance = axiosWithoutToken;
+    return axiosInstance.get(`/cities`);
+  };
+
+  const getCityById2 = (cityId?: number): Promise<AxiosResponse<any>> => {
+    let axiosInstance: AxiosInstance = axiosWithoutToken;
+    return axiosInstance.get(`/cities/${cityId || 1}`);
+  };
+
   return {
     getCities,
     getCityById,
     deleteCity,
+    getCities2,
+    getCityById2,
   };
 };
