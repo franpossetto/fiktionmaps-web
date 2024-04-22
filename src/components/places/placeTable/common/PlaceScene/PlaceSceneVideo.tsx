a833ceb17c62c744339b285251efa372d9cee2d9
TATICO\franc <franciscopossetto@hotmail.com>
2024-04-22 16:08:23 -0300

    added placeSceneImage

--- src/components/places/placeTable/common/PlaceScene/PlaceSceneVideo.tsx
+++ src/components/places/placeTable/common/PlaceScene/PlaceSceneVideo.tsx
@@ -3,12 +3,22 @@ import { useEffect, useState } from "react";
 import { storage } from "../../../../../config/firebase";
 import { PlayCircleIcon } from "@heroicons/react/24/solid";
 
-interface PlaceImageSceneProps {
+interface PlaceSceneVideoProps {
   imgUrl?: string;
 }
 
-export const PlaceSceneImage: React.FC<PlaceImageSceneProps> = ({ imgUrl }) => {
+export const PlaceSceneVideo: React.FC<PlaceSceneVideoProps> = ({ imgUrl }) => {
   const [imageUrl, setImageUrl] = useState<string | undefined>();
+
+  const [showVideo, setShowVideo] = useState(false);
+
+  const handleVideoOpen = () => {
+    setShowVideo(true);
+  };
+
+  const handleVideoClose = () => {
+    setShowVideo(false);
+  };
 
   useEffect(() => {
     const fetchImage = () => {
@@ -28,6 +38,9 @@ export const PlaceSceneImage: React.FC<PlaceImageSceneProps> = ({ imgUrl }) => {
   return (
     <div className="relative flex flex-row h-28 w-44 overflow-hidden shadow-sm">
       <img className="object-cover w-full h-full" src={imageUrl} alt="" />
+      <button className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-10 bg-transparent hover:scale-110 hover:shadow-lg transition-all">
+        <PlayCircleIcon className="h-8 w-8 text-white/70" />
+      </button>
     </div>
   );
 };
