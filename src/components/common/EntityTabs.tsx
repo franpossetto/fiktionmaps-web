d02ccc35abc0d01ab575de1115f2256487e4121d
TATICO\franc <franciscopossetto@hotmail.com>
2024-05-05 10:22:59 -0300

    added function to handle tab selection

--- src/components/common/EntityTabs.tsx
+++ src/components/common/EntityTabs.tsx
@@ -4,6 +4,7 @@ import { Link, useLocation } from "react-router-dom";
 export interface Tab {
   label: string;
   key: string;
+  onClick: any;
   icon: any;
 }
 
@@ -15,22 +16,27 @@ const EntityTabs: React.FC<EntityTabsProps> = ({ tabs }) => {
   const location = useLocation();
   const basePath = "/collaboration/places";
 
+  const isSelectedTab = (tabKey: string) => {
+    const currentPath =
+      location.pathname === "/collaboration"
+        ? "/collaboration/mine"
+        : location.pathname;
+    const isSelected = tabKey === currentPath.split("/").pop();
+    const className = `
+      rounded-lg text-xs mt-5 px-2 py-2 items-center justify-start
+      ${
+        isSelected
+          ? "bg-gray-900 text-white dark:bg-gray-100 dark:text-black"
+          : "bg-gray-100 text-black dark:bg-gray-800 dark:text-white"
+      }
+    `;
+    return className;
+  };
+
   return (
     <div className="flex flex-row">
       {tabs.map((tab, index) => (
-        <button
-          key={index}
-          type="button"
-          className={`
-          rounded-lg text-xs mt-5 px-2 py-2 items-center justify-start
-          ${
-            tab.key === location.pathname.split("/").pop()
-              ? "bg-gray-900 text-white dark:bg-gray-100 dark:text-black"
-              : "bg-gray-100 text-black dark:bg-gray-800 dark:text-white"
-          }
-          ${index < tabs.length - 1 ? "mr-3" : ""}
-        `}
-        >
+        <button key={index} type="button" className={isSelectedTab(tab.key)}>
           <span className="flex items-center">
             {tab.icon}
             <Link to={`${basePath}/${tab.key}`}>{tab.label}</Link>
--- src/pages/collaboration/CollaborationView.tsx
+++ src/pages/collaboration/CollaborationView.tsx
@@ -1,16 +1,11 @@
-import { Fragment, useState } from "react";
-import { Dialog, Switch, Transition } from "@headlessui/react";
-import PlaceTableCollab from "./places/PlaceTableSections/PlaceTableCollab";
 import { CollaborationNavBar } from "./CollaborationNavBar";
-import { BrowserRouter, Outlet, Route, Router, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import { FictionTableCollab } from "./fictions/FictionTableCollab";
 import { SceneTableCollab } from "./scenes/SceneTableCall";
 import { CityTableCollab } from "./cities/CityTableCollab";
 import { PlaceTableView } from "./places/PlaceTableView";
 
 export default function CollaborationView() {
-  const [sidebarOpen, setSidebarOpen] = useState(false);
-
   return (
     <>
       <div className="lg:pl-20 bg-white dark:bg-gray-900">
